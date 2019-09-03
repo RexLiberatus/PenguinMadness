b1ecf0c1890b3b949ff0b6370a3b7968e90a37ad
Rex <RexLiberatus@users.noreply.github.com>
2019-09-03 15:09:46 +0200

    integrated basic ground and hand collider. Also Listing every tiles composing the playground

---
+++ ARProject/Assets/Playground/Scripts/ListTilesManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTilesManager : MonoBehaviour
+{
+    [SerializeField]
+    List<PenguinPresence> tileList=new List<PenguinPresence>();
+
+    private void Start()
+    {
+        //PenguinPresence[] TilePointer= FindObjectsOfType<PenguinPresence>();
+        tileList.AddRange(FindObjectsOfType<PenguinPresence>());
+       //foreach(PenguinPresence element in TilePointer)
+       // {
+       //     tileList.Add(element.gameObject);
+       // }
+    }
+}
